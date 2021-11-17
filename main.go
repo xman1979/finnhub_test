2e8c1faed5bb30a2ad1b5d62f618926ed6ed1de0
xiaodongma <xiaodongma@devbig404.ftw3.facebook.com>
2021-11-17 11:01:39 -0800

    benchmark a few more

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"context"
 	"io/ioutil"
+	"flag"
+	"os"
 	"time"
 	"strings"
 	"sync"
@@ -13,6 +15,9 @@ import (
 
 var (
 	api *finnhub.DefaultApiService // Singleton, funnhub API instance
+)
+var (
+	endpoint = flag.String("endpoint", "quote", "Finnhub endpoint [quote|candle|bf]")
 )
 
 func getFinnhubAPI() (*finnhub.DefaultApiService, error) {
@@ -44,7 +49,46 @@ func getStockQuote(ctx context.Context, finnhubSymbol string) (string, error) {
 	return fmt.Sprintf("%3.2f", quote.GetC()), nil
 }
 
+func getStockCandles(ctx context.Context, finnhubSymbol string) (string, error) {
+	api, err := getFinnhubAPI()
+	if err != nil {
+		return "", err
+	}
+	resolution := "15" // every 15 minutes
+	to := time.Now()
+	from := to.Add(-1*time.Duration(1*24*time.Hour))
+	candles, _, err := api.StockCandles(ctx).Symbol(finnhubSymbol).Resolution(resolution).From(from.Unix()).To(to.Unix()).Execute()
+	if err != nil {
+		return "", err
+	}
+	if !candles.HasC() {
+		return "", fmt.Errorf("current price is missing for symbol %s", finnhubSymbol)
+	}
+	return fmt.Sprintf("%3.2f", candles.GetC()[0]), nil
+}
+
+func getStockBasicFinancials(ctx context.Context, finnhubSymbol string) (string, error) {
+	api, err := getFinnhubAPI()
+	if err != nil {
+		return "", err
+	}
+	bf, _, err := api.CompanyBasicFinancials(ctx).Symbol(finnhubSymbol).Metric("all").Execute()
+	if err != nil {
+		return "", err
+	}
+	if !bf.HasMetric() {
+		return "", fmt.Errorf("metric is missing for symbol %s", finnhubSymbol)
+	}
+	metric := bf.GetMetric()
+	week52High := ""
+	if val, ok := metric["52WeekHigh"]; ok {
+		week52High = fmt.Sprintf("%v", val)
+	}
+	return week52High, nil
+}
+
 func main() {
+	flag.Parse()
 	ctx := context.Background()
 
 	input := make(chan string, 10)
@@ -52,6 +96,7 @@ func main() {
 	var countTotal uint64
 	var countSucceed uint64
 	start := time.Now()
+	var err error
 
 	for i := 0; i < 1000; i++ {
 		wg.Add(1)
@@ -60,16 +105,26 @@ func main() {
 				wg.Done()
 			}()
 			for symbol := range input {
-				_, err := getStockQuote(ctx, symbol)
+				beginRequest := time.Now()
+				switch(*endpoint) {
+				case "quote":
+					_, err = getStockCandles(ctx, symbol)
+				case "candle":
+					_, err = getStockCandles(ctx, symbol)
+				case "bf":
+					_, err = getStockBasicFinancials(ctx, symbol)
+				}
 				if err != nil {
-					fmt.Printf("failed to get stock name from company profile, err = %s\n", err)
+					fmt.Printf("err = %s\n", err)
+					os.Exit(1)
 				} else {
 					atomic.AddUint64(&countSucceed, 1)
 				}
+				requestTime := time.Since(beginRequest).Milliseconds()
 				if countSucceed % 100 == 0 && countSucceed > 0 {
 					delta := time.Since(start).Seconds()
-					fmt.Printf("successfully get name for %d symbols, QPS = %3.2f\n",
-						countSucceed, float64(countSucceed)/delta)
+					fmt.Printf("successfully get name for %d symbols, QPS = %3.2f, request time %d ms for last symbol %s\n",
+						countSucceed, float64(countSucceed)/delta, requestTime, symbol)
 				}
 			}
 		}()
